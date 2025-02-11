7592ff54a6c99c5f1155b065bfb7436ed6cf43c4
PrakharSuraj <prakharsurajwipro@gmail.com>
2025-02-11 19:13:28 +0530

    Day-9 SpringBoot

---
+++ SBA--2/src/main/java/com/example/SBA/Sba2Application.java
@@ -0,0 +1,16 @@
+package com.example.SBA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sba2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sba2Application.class, args);
+		System.out.println("Hello");
+		System.out.println("Hello World");
+		System.out.println("Hello K");
+	}
+
+}
