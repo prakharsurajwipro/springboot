877f5585e9328d419bec90b13c8672454eab9b16
PrakharSuraj <prakharsurajwipro@gmail.com>
2025-02-11 12:21:31 +0530

    Day-9 SpringBoot

---
+++ scheduling-task/src/test/java/com/example/scheduling_task/SchedulingTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scheduling_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
