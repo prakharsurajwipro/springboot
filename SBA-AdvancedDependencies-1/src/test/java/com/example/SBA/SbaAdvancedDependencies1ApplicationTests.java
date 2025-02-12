9bc6b670a16f69bd487c5cdb0d0debb1d8c7f93d
PrakharSuraj <prakharsurajwipro@gmail.com>
2025-02-12 15:22:11 +0530

    Day-10 SpringBoot

---
+++ SBA-AdvancedDependencies-1/src/test/java/com/example/SBA/SbaAdvancedDependencies1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SBA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbaAdvancedDependencies1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
